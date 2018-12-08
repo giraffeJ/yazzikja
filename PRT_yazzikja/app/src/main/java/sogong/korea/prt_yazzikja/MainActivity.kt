ca0dd6a3a289a2ab8f8faa49897912d191ce3474
giraffeJ <35956557+giraffeJ@users.noreply.github.com>
2018-12-08 20:27:31 +0900

    10일차 중간 에러있음.MainActivity.kt:82, 영훈이꺼랑 일부 합침. drawable, splash

--- PRT_yazzikja/app/src/main/java/sogong/korea/prt_yazzikja/MainActivity.kt
+++ PRT_yazzikja/app/src/main/java/sogong/korea/prt_yazzikja/MainActivity.kt
@@ -1,22 +1,31 @@
 package sogong.korea.prt_yazzikja
 
 import android.Manifest
+import android.app.Activity
 import android.content.Intent
 import android.content.pm.PackageManager
+import android.net.Uri
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.design.widget.BottomNavigationView
 import android.support.v4.app.ActivityCompat
 import android.support.v4.content.ContextCompat
 import android.view.MenuItem
+import com.google.android.gms.tasks.Continuation
+import com.google.android.gms.tasks.Task
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.UploadTask
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemSelectedListener {
+    var PICK_PROFILE_FROM_ALBUM = 10
     override fun onNavigationItemSelected(item: MenuItem): Boolean {
         when(item.itemId){
             R.id.action_home -> {
-                var detailViewFragment = DetailViewFragment()
-                supportFragmentManager.beginTransaction().replace(R.id.main_content,detailViewFragment).commit()
+                var detailviewFragment = DetailViewFragment()
+                supportFragmentManager.beginTransaction().replace(R.id.main_content,detailviewFragment).commit()
                 return true
             }
             R.id.action_search -> {
@@ -25,10 +34,9 @@ class MainActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemS
                 return true
             }
             R.id.action_add_photo -> {
-                if(ContextCompat.checkSelfPermission(this,Manifest.permission.READ_EXTERNAL_STORAGE)==PackageManager.PERMISSION_GRANTED){ //저장소 접근 허용한 경우에만.
-                    startActivity(Intent(this, AddPhotoActivity::class.java))
+                if(ContextCompat.checkSelfPermission(this,Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED){
+                    startActivity(Intent(this,AddPhotoActivity::class.java))
                 }
-
                 return true
             }
             R.id.action_favorite_alarm -> {
@@ -43,15 +51,38 @@ class MainActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemS
             }
         }
         return false
-    }
 
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
         bottom_navigation.setOnNavigationItemSelectedListener(this)
         bottom_navigation.selectedItemId = R.id.action_home
-
         ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE),1)
+    }
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if(requestCode == PICK_PROFILE_FROM_ALBUM && resultCode == Activity.RESULT_OK){
+            var imageUrl = data?.data
+            var uid = FirebaseAuth.getInstance().currentUser!!.uid
+            var profileimageRef = FirebaseStorage.getInstance().reference.child("userProfileImages").child(uid)
+            var map = HashMap<String,Any>()
+            profileimageRef.putFile(imageUrl!!).continueWithTask(Continuation<UploadTask.TaskSnapshot,Task<Uri>> { task ->
+                if (!task.isSuccessful) {
+                    task.exception?.let {
+                        throw it
+                    }
+                }
+                return@Continuation profileimageRef.downloadUrl
+            }).addOnCompleteListener { task ->
+                val downloadUri = task.result
+                map["image"] = downloadUri
+                println("에러체크"+map)
+                FirebaseFirestore.getInstance().collection("profileimages").document(uid).update(map)
+            }
+            println("에러체크"+map)
+
+        }
     }
 }
--- PRT_yazzikja/app/src/main/java/sogong/korea/prt_yazzikja/UserFragment.kt
+++ PRT_yazzikja/app/src/main/java/sogong/korea/prt_yazzikja/UserFragment.kt
@@ -1,13 +1,24 @@
 package sogong.korea.prt_yazzikja
 
+import android.content.Intent
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import kotlinx.android.synthetic.main.fragment_user.view.*
 
 class UserFragment : Fragment(){
+    var fragmentView : View? = null
+    var PICK_PROFILE_FOR_ALBUM = 10
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return LayoutInflater.from(inflater.context).inflate(R.layout.fragment_user,container,false)
+        fragmentView = inflater.inflate(R.layout.fragment_user,container, false)
+        fragmentView?.account_iv_profile?.setOnClickListener {
+            var photoPickerIntent = Intent(Intent.ACTION_PICK)
+            photoPickerIntent.type = "image/*"
+            activity?.startActivityForResult(photoPickerIntent, PICK_PROFILE_FOR_ALBUM)
+        }
+        return fragmentView
     }
+
 }
